64c9375a3ed8b076130f6cd6f07c64622257be7c
Ampelini <s333745@oslomet.no>
2019-09-10 11:26:41 +0200

    foreløpig arbeid på oppgave1 (ikke ferdig)

--- Oblig1/src/no/oslomet/cs/algdat/Oblig1/Oblig1.java
+++ Oblig1/src/no/oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -1,24 +1,118 @@
 package no.oslomet.cs.algdat.Oblig1;
 
 ////// Løsningsforslag Oblig 1 - 2019 ////////////////////////
+/**
+ * Camilla Hoelgaard - s333783
+ * Maria Teresita Halvorsen - s326325
+ * Signe Aanderaa Eide - s333781
+ * Ana-Maria Poljac - s333745
+ */
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.Arrays;
 import java.util.NoSuchElementException;
+import java.util.Random;
 
 
 public class Oblig1 {
+
+
     private Oblig1() {
     }
 
+    // metode som bytter plass på verdiene når den kalles
+    public static void bytt(int[] a, int i, int j){
+        int temp = a[i]; a[i] = a[j]; a[j] = temp;
+    }
+
+    /** Oppg.1 :
+     * Gitt en tabell med tallene fra 1 til n vil det være :
+     * Flest ombyttinger: når største verdi er på indeks 0
+     * Færrest ombyttinger: omvendt av over - når største verdi er på siste indeks
+     * I gjennomsnitt:
+     */
+
     ///// Oppgave 1 //////////////////////////////////////
     public static int maks(int[] a) {
-        throw new NotImplementedException();
+
+        //throw new NotImplementedException(); // TODO: se på denne (original)
+
+        if(a.length < 1){
+            throw new NoSuchElementException("Tabellen er tom");
+        }
+
+        for(int i= 0; i < a.length-1; i++){
+            if(a[i] > a[i+1]){
+                bytt(a,i,i+1);
+            }
+        }
+        return a[a.length-1];
+    }
+
+    // Henter inn hjelpemetoder
+    public static void randPerm(int[] a) {
+        Random r = new Random();     // en randomgenerator
+
+        for (int k = a.length - 1; k > 0; k--) {
+            int i = r.nextInt(k + 1);  // tilfeldig tall fra [0,k]
+            bytt(a, k, i);
+        }
+    }
+
+    public static int[] randPerm(int n) { // stokker om a
+        Random r = new Random();         // en randomgenerator (hentes fra java.util)
+        int[] a = new int[n];            // en tabell med plass til n tall
+
+        Arrays.setAll(a, i -> i + 1);    // legger inn tallene 1, 2, . , n
+
+        for (int k = n - 1; k > 0; k--){ // løkke som går n - 1 ganger
+
+            int i = r.nextInt(k+1);        // en tilfeldig tall fra 0 til k [0,k]
+            bytt(a,k,i);                   // bytter om
+        }
+
+        return a;                        // permutasjonen returneres
     }
 
     public static int ombyttinger(int[] a) {
-        throw new NotImplementedException();
+
+        //throw new NotImplementedException(); // TODO: se på denne (original)
+
+        int teller = 0;
+        randPerm(a);
+
+        for(int i=0; i < a.length -1; i++){
+            if(a[i] > a[i+1]){
+                bytt(a,i,i+1);
+                teller++;
+            }
+        }
+        return teller; // Returnerer antall ombyttinger
+    }
+
+    // Egendef metode
+    public static int gjennomsnitt(int[] a){
+        int sum = 0;
+
+        for(int i = 0; i < a.length; i++){
+            sum += ombyttinger(a);
+        }
+        int snitt = sum / a.length;
+        return snitt;
+    }
+
+    // For testing per nå - endres / fjernes
+    public static void main(String[] args) {
+        int[] a = {1,2,11,4,2,6,7,13,9,10};
+        int[] b = randPerm(20); // TODO: Bør defineres som egen metode alt. i test klassen
+
+        System.out.println("Maks verdi er: "+ maks(a));
+        System.out.println("Array det jobbes på: "+Arrays.toString(a));
+        System.out.println("-------------------------------------------------");
+        System.out.println("Antall ombyttinger :"+ombyttinger(b));
+        System.out.println("Arrayet det jobbes på:"+Arrays.toString(b));
+        System.out.println("Gjennomsnittlig ombyttinger er "+gjennomsnitt(b));
     }
 
     ///// Oppgave 2 //////////////////////////////////////
